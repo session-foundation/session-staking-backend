5d549b5c43f5f9c489a6cd763ede45e0ccb81dec
Ryan Miller <aerilym@pm.me>
2024-11-20 17:28:36 +1100

    feat: read only api

---
+++ api.py
@@ -0,0 +1,300 @@
+#!/usr/bin/env python3
+import flask
+import time
+
+import eth_utils
+import subprocess
+
+from blackd import handle
+
+import config
+from werkzeug.routing import BaseConverter
+from db.read import DBReader
+from log import Log
+from oxen.rpc import OxenRPC
+from util.data import DataManager
+from util.parse import eth_regex, Hex64Converter, hexify, EthConverter
+
+
+class WalletInfo:
+    def __init__(self):
+        self.rewards = 0  # Atomic SENT
+        self.contract_rewards = 0
+        self.contract_claimed = 0
+
+
+class App(flask.Flask):
+    def __init__(self, name):
+        super().__init__(__name__)
+        log = Log(name, enable_perf=config.backend.performance_logging)
+        log.set_level(config.backend.log_level)
+        git_rev = subprocess.run(
+            ["git", "rev-parse", "--short=9", "HEAD"], stdout=subprocess.PIPE, text=True
+        )
+        self.git_rev = git_rev.stdout.strip() if git_rev.returncode == 0 else "(unknown)"
+
+        # Creates a generic logger to pipe other packages logs into the main app logger
+        generic_logger = Log(None)
+        generic_logger.set_level(
+            config.backend.log_level_generic
+            if config.backend.log_level_generic is not None
+            else config.backend.log_level
+        )
+        self.log = log.logger
+
+        self.db_reader = DBReader(
+            db_path=config.backend.sqlite_db,
+            log_level=config.backend.log_level,
+            perf=config.backend.performance_logging,
+        )
+
+        rpc_url = config.backend.rpc_api if config.backend.rpc_api else config.backend.rpc_shared
+        rpc_cache = (
+            config.backend.rpc_api_cache
+            if config.backend.rpc_api_cache
+            else config.backend.rpc_shared_cache
+        )
+
+        self.rpc = OxenRPC(
+            self.log,
+            rpc_url,
+            rpc_cache,
+        )
+        self.loop_sleep_refresh_rate_seconds = rpc_cache if rpc_cache > 0 else 5
+
+        self.data = DataManager(stale_time_seconds=config.backend.stale_time_seconds)
+
+        self.allowed_contract_names = set()
+
+
+app = App(config.backend.api_name if config.backend.api_name else __name__)
+
+
+def get_and_refresh_allowed_contract_names():
+    allowed_contract_names = set()
+    for name in app.db_reader.get_smart_contract_names():
+        allowed_contract_names.add(name)
+    app.allowed_contract_names = allowed_contract_names
+    return allowed_contract_names
+
+
+app.url_map.converters["hex64"] = Hex64Converter
+app.url_map.converters["eth_wallet"] = EthConverter
+
+
+def json_response(vals):
+    """
+    Takes a dict, adds some general info fields to it, and jsonifies it for a flask route function
+    return value.  The dict gets passed through `hexify` first to convert any bytes values to hex.
+    """
+    hexify(vals)
+    network = app.data.get("network_info", getter=app.db_reader.get_network_info)
+    return flask.jsonify({**vals, "network": network, "t": time.time()})
+
+
+@app.route("/info")
+def get_network_info():
+    return json_response({})
+
+
+def get_nodes_cached():
+    return app.data.get("nodes", getter=app.db_reader.get_nodes)
+
+
+@app.route("/nodes")
+def get_nodes():
+    return json_response({"nodes": app.data.get("nodes", getter=app.db_reader.get_nodes)})
+
+
+"""
+//////////////////////////////////////////////////////////////
+//                                                          //
+//                     Stake Endpoints                      //
+//                                                          //
+//////////////////////////////////////////////////////////////
+"""
+
+
+# TODO: might make sense to investigate storing contributor and operator addresses in the db as blobs and compare with bytes
+@app.route("/stakes/<eth_wallet:eth_wal>")
+@app.route("/nodes/<eth_wallet:eth_wal>")
+def get_stakes(eth_wal: str):
+    try:
+        if not eth_wal or not eth_utils.is_address(eth_wal):
+            raise ValueError("Invalid wallet address")
+
+        nodes = get_nodes_cached()
+
+        related_nodes = []
+        for node in nodes:
+            if node.operator_address == eth_wal:
+                related_nodes.append(node)
+            elif node.contributors is not None:
+                for contributor in node.contributors:
+                    if contributor.address == eth_wal:
+                        related_nodes.append(node)
+
+        return json_response({"stakes": related_nodes})
+
+    except ValueError as e:
+        app.logger.error(f"Exception: {e}")
+        return flask.abort(400, e)
+    except Exception as e:
+        app.logger.error(f"Exception: {e}")
+        return flask.abort(500, e)
+
+
+"""
+//////////////////////////////////////////////////////////////
+//                                                          //
+//                   Contract Endpoints                     //
+//                                                          //
+//////////////////////////////////////////////////////////////
+"""
+
+
+def get_cached_allowed_contract_names():
+    return app.data.get(
+        "allowed_contract_names",
+        getter=get_and_refresh_allowed_contract_names,
+        ttl=config.backend.stale_time_seconds_contract_abis,
+    )
+
+
+@app.route("/contract/names")
+def get_abi_names():
+    return json_response({"names": list(get_cached_allowed_contract_names())})
+
+
+@app.route("/contract/abis")
+def get_abis():
+    return json_response(
+        {"abis": app.data.get("abis", getter=app.db_reader.get_smart_contract_abis)}
+    )
+
+
+@app.route("/contract/addresses")
+def get_contract_addresses():
+    return json_response(
+        {"addresses": app.data.get("addresses", getter=app.db_reader.get_smart_contract_addresses)}
+    )
+
+
+@app.route("/contract/contribution")
+def get_open_contract_details():
+    return json_response(
+        {"contracts": app.data.get("contracts", getter=app.db_reader.get_contribution_contracts)}
+    )
+
+
+@app.route("/contract/abi/<contract_name>")
+def get_abi(contract_name: str):
+    if contract_name not in get_cached_allowed_contract_names():
+        return flask.abort(404, f"Contract {contract_name} not found")
+
+    return json_response(
+        {
+            "contract": app.data.get(
+                "abi", getter=app.db_reader.get_smart_contract_abi, getter_args=contract_name
+            )
+        }
+    )
+
+
+@app.route("/contract/address/<contract_name>")
+def get_contract_address(contract_name: str):
+    if contract_name not in get_cached_allowed_contract_names():
+        return flask.abort(404, f"Contract {contract_name} not found")
+
+    return json_response(
+        {
+            "address": app.data.get(
+                "address",
+                getter=app.db_reader.get_smart_contract_address,
+                getter_args=contract_name,
+            )
+        }
+    )
+
+
+"""
+//////////////////////////////////////////////////////////////
+//                                                          //
+//                     Exit Endpoints                       //
+//                                                          //
+//////////////////////////////////////////////////////////////
+"""
+
+
+def handle_get_exit_and_liquidation(ed25519_pubkey: bytes, liquidate: bool):
+    try:
+        response = app.rpc.bls_exit_liquidation_request(ed25519_pubkey, liquidate).get()
+        if response is None:
+            return flask.abort(504)  # Gateway timeout
+        if "status" in response:
+            response.pop("status")
+        result = json_response({"result": response})
+        return result
+    except TimeoutError:
+        return flask.abort(408)  # Request timeout
+
+
+@app.route("/exit/<hex64:ed25519_pubkey>")
+def get_exit(ed25519_pubkey: bytes):
+    return handle_get_exit_and_liquidation(ed25519_pubkey, liquidate=False)
+
+
+@app.route("/liquidation/<hex64:ed25519_pubkey>")
+def get_liquidation(ed25519_pubkey: bytes):
+    return handle_get_exit_and_liquidation(ed25519_pubkey, liquidate=True)
+
+
+@app.route("/exit_liquidation_list")
+def get_exit_liquidation_list():
+    # TODO: add exit list management to main.py and main database and implement db_reader.get_exitable_nodes
+    # return json_response(
+    #     {"result": app.data.get("exit_liquidation_list", getter=app.db_reader.get_exitable_nodes)}
+    # )
+    return json_response({"result": []})
+
+
+"""
+//////////////////////////////////////////////////////////////
+//                                                          //
+//                   Rewards Endpoints                      //
+//                                                          //
+//////////////////////////////////////////////////////////////
+"""
+
+
+@app.route("/rewards/<eth_wallet:eth_wal>", methods=["GET", "POST"])
+def get_rewards(eth_wal: str):
+    if flask.request.method == "GET":
+        # TODO: implement db_reader.get_rewards_for_wallet and add rewards to main.py and main database
+        # return json_response(
+        #     {"result": app.data.get(f"rewards-{eth_wal}", getter=app.db_reader.get_rewards_for_wallet, getter_args=eth_wal)}
+        # )
+        return json_response({})
+
+    if flask.request.method == "POST":
+        try:
+            response = app.rpc.bls_rewards_request(eth_utils.to_checksum_address(eth_wal)).get()
+            if response is None:
+                return flask.abort(504)  # Gateway timeout
+            if "status" in response:
+                response.pop("status")
+            if "address" in response:
+                response.pop("address")
+            result = json_response({"result": response})
+            return result
+        except TimeoutError:
+            return flask.abort(408)  # Request timeout
+
+    return flask.abort(405)  # Method not allowed
+
+
+def bootstrap():
+    get_and_refresh_allowed_contract_names()
+
+
+bootstrap()
