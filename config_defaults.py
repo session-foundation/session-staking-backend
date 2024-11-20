e7e14c2adc30277ea519c0ac4e1c6b18ef0c4f3a
Ryan Miller <aerilym@pm.me>
2024-11-20 17:28:08 +1100

    feat: overhaul config

--- config_defaults.py
+++ config_defaults.py
@@ -15,52 +15,90 @@ import logging
 
 B58_ALPHABET = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
 
+
 class Backend:
-    sqlite_db:               str = 'sent-backend.db'
-    thread_pool_max_workers: int = 10
-    stale_time_seconds:      int = 30
-    rpc:                     str = ''
-    oxen_wallet_regex:       str = ''
-    reward_rate_pool_addr:   str = '0x0000000000000000000000000000000000000000'
-    sn_contrib_factory_addr: str = '0x0000000000000000000000000000000000000000'
-    sn_rewards_addr:         str = '0x0000000000000000000000000000000000000000'
-    sn_token_addr:           str = '0x0000000000000000000000000000000000000000'
-    provider_url:            str = 'http://localhost:8545' # Default hardhat private chain node address
-    log_level                    = logging.INFO
+    """
+    SHARED CONFIG
+    """
+    log_level                           = logging.INFO
+    log_level_generic                   = None  # Logs from other packages will use log_level if this is not set
+    oxen_wallet_regex:              str = ""
+    sqlite_db:                      str = "sent-backend.db"
+    sqlite_schema:                  str = "db/schema.sql"
+    rpc_shared:                     str = ""
+    rpc_shared_cache:               int = 2
+
+    """
+    API CONFIG
+    """
+    api_name:                       str = "api"
+    rpc_api:                        str = ""
+    rpc_api_cache:                  int = 2
+    """
+    REGISTRATION API CONFIG
+    """
+    registration_api_name:          str = "registration_api"
+    registration_sqlite_db:         str = "sent-backend-registrations.db"
+    registration_sqlite_schema:     str = "registration/schema.sql"
+
+    """
+    FETCHER CONFIG
+    """
+    abi_dir = "web3client/abis"
+    # Arbitrum runs at ~4 blocks per second, and the rpc node has a limit of 30m, so scan for 120 blocks
+    arbitrum_rescan_safety_blocks: int = 120
+    addr_reward_rate_pool:         str = "0x0000000000000000000000000000000000000000"
+    addr_sent:                     str = "0x0000000000000000000000000000000000000000"
+    addr_sn_contrib:               str = "0x0000000000000000000000000000000000000000"
+    addr_sn_contrib_factory:       str = "0x0000000000000000000000000000000000000000"
+    addr_sn_rewards:               str = "0x0000000000000000000000000000000000000000"
+    refresh_rate_seconds_arbitrum: int = 30
+    max_time_keeper_events:        int = 10_000
+    fetcher_name:                  str = "fetcher"
+    performance_logging:          bool = False
+    rpc_fetcher:                   str = ""
+    rpc_fetcher_cache:             int = 2
+    stale_time_seconds:            int = 30
+    stale_time_seconds_contract_abis: int = 300
+    thread_pool_max_workers:       int = 50
+    web3_caller_address:    str | None = None
+    web3_private_key:       str | None = None
+    web3_provider_url:      str = "http://localhost:8545"  # Default hardhat private chain node address)
+
 
 # Session mainnet contracts
-mainnet_backend                          = Backend()
-mainnet_backend.sqlite_db                = 'ssb-mainnet.db'
-mainnet_backend.rpc                      = 'ipc://oxend/mainnet.sock'
-mainnet_backend.oxen_wallet_regex        = f'L[{B58_ALPHABET}]{{94}}"'
+mainnet_backend = Backend()
+mainnet_backend.oxen_wallet_regex      = f'L[{B58_ALPHABET}]{{94}}"'
+mainnet_backend.rpc_shared             = "ipc://oxend/mainnet.sock"
+mainnet_backend.sqlite_db              = "ssb-mainnet.db"
 
 # Session testnet contracts
-testnet_backend                          = Backend()
-testnet_backend.sqlite_db                = 'ssb-testnet.db'
-testnet_backend.rpc                      = 'ipc://oxend/testnet.sock'
-testnet_backend.oxen_wallet_regex        = f"T[{B58_ALPHABET}]{{96}}"
+testnet_backend = Backend()
+testnet_backend.oxen_wallet_regex       = f"T[{B58_ALPHABET}]{{96}}"
+testnet_backend.rpc_shared              = "ipc://oxend/testnet.sock"
+testnet_backend.sqlite_db               = "ssb-testnet.db"
 
 # Session devnet.v3 contracts
-devnet_backend                           = Backend()
-devnet_backend.sqlite_db                 = 'ssb-devnet.db'
-devnet_backend.rpc                       = 'ipc://oxend/devnet.sock'
-devnet_backend.oxen_wallet_regex         = f"dV[{B58_ALPHABET}]{{95}}"
-devnet_backend.reward_rate_pool_addr     = '0xb515C61DE12f28eE908a905b930aFb80B9bAd7cf'
-devnet_backend.sn_contrib_factory_addr   = '0x0000000000000000000000000000000000000000'
-devnet_backend.sn_rewards_addr           = '0x75Dc11700b2D03902FCb5Ca7aFd6A859a1Fa25Cb'
-devnet_backend.sn_token_addr             = '0x0000000000000000000000000000000000000000'
-devnet_backend.provider_url              = 'https://sepolia-rollup.arbitrum.io/rpc'
+devnet_backend = Backend()
+devnet_backend.addr_reward_rate_pool    = "0xb515C61DE12f28eE908a905b930aFb80B9bAd7cf"
+devnet_backend.addr_sn_contrib          = "0x0000000000000000000000000000000000000000"
+devnet_backend.addr_sn_contrib_factory  = "0x0000000000000000000000000000000000000000"
+devnet_backend.addr_sn_rewards          = "0x75Dc11700b2D03902FCb5Ca7aFd6A859a1Fa25Cb"
+devnet_backend.oxen_wallet_regex        = f"dV[{B58_ALPHABET}]{{95}}"
+devnet_backend.rpc_shared               = "ipc://oxend/devnet.sock"
+devnet_backend.sqlite_db                = "ssb-devnet.db"
+devnet_backend.web3_provider_url        = "https://sepolia-rollup.arbitrum.io/rpc"
 
 # Session stagenet.v3 contracts
-stagenet_backend                         = Backend()
-stagenet_backend.sqlite_db               = 'ssb-stagenet.db'
-stagenet_backend.rpc                     = 'ipc://oxend/stagenet.sock'
-stagenet_backend.oxen_wallet_regex       = f"ST[{B58_ALPHABET}]{{95}}"
-stagenet_backend.reward_rate_pool_addr   = '0x38cD8D3F93d591C18cf26B3Be4CB2c872aC37953'
-stagenet_backend.sn_contrib_factory_addr = '0x66d0D4f71267b3150DafF7bD486AC5E097E7E4C6'
-stagenet_backend.sn_rewards_addr         = '0x4abfFB7f922767f22c7aa6524823d93FDDaB54b1'
-stagenet_backend.sn_token_addr           = '0x70c1f36C9cEBCa51B9344121D284D85BE36CD6bB'
-stagenet_backend.provider_url            = 'https://sepolia-rollup.arbitrum.io/rpc'
+stagenet_backend = Backend()
+stagenet_backend.addr_reward_rate_pool = "0x38cD8D3F93d591C18cf26B3Be4CB2c872aC37953"
+stagenet_backend.addr_sn_contrib       = "0x70c1f36C9cEBCa51B9344121D284D85BE36CD6bB"
+stagenet_backend.addr_sn_contrib_factory = "0x66d0D4f71267b3150DafF7bD486AC5E097E7E4C6"
+stagenet_backend.addr_sn_rewards       = "0x4abfFB7f922767f22c7aa6524823d93FDDaB54b1"
+stagenet_backend.oxen_wallet_regex     = f"ST[{B58_ALPHABET}]{{95}}"
+stagenet_backend.rpc_shared            = "tcp://localhost:6786"
+stagenet_backend.sqlite_db             = "ssb-stagenet.db"
+stagenet_backend.web3_provider_url     = "http://10.24.0.1/arb_sepolia"
 
 # Assign the active backend to be used in the sent-staking-backend
-backend                                  = stagenet_backend
+backend = stagenet_backend
---
+++ config_validate.py
@@ -0,0 +1,79 @@
+import logging
+
+import config
+from log import Log
+from oxen.omq import omq_connection
+from oxen.rpc import OxenRPC
+from util import is_not_empty_string, valid_address_assertion
+
+
+def validate_config(conf: config):
+    log = Log("config_validate").logger
+    log.perf.start("validate_config")
+    log.info("Validating config")
+
+    """
+    Production warnings
+    """
+    if conf.backend.log_level < logging.INFO:
+        log.warning(
+            "Log level is set to {} which is less than INFO. This is not recommended for production.".format(
+                conf.backend.log_level
+            )
+        )
+    elif conf.backend.log_level > logging.ERROR:
+        log.warning(
+            "Log level is set to {} which is greater than ERROR. This is not recommended for production.".format(
+                conf.backend.log_level
+            )
+        )
+
+    if conf.backend.performance_logging:
+        log.warning("Performance logging is enabled. This is not recommended for production.")
+
+    """
+    Validations
+    """
+
+    assert is_not_empty_string(conf.backend.sqlite_db), "sqlite_db is not set in config.py"
+    assert is_not_empty_string(conf.backend.rpc_fetcher), "rpc is not set in config.py"
+    assert is_not_empty_string(
+        conf.backend.oxen_wallet_regex
+    ), "oxen_wallet_regex is not set in config.py"
+
+    # Assert all contract addresses are valid
+    valid_address_assertion(conf.backend.addr_sn_contrib, "addr_sn_contrib")
+    valid_address_assertion(conf.backend.addr_sent, "addr_sent")
+    valid_address_assertion(conf.backend.addr_sn_rewards, "addr_sn_rewards")
+    valid_address_assertion(conf.backend.addr_reward_rate_pool, "addr_reward_rate_pool")
+
+    assert is_not_empty_string(
+        conf.backend.web3_provider_url
+    ), "web3_provider_url is not set in config.py"
+
+    """
+    Web3 client validations
+    """
+    # web3_client = Web3Client(
+    #     conf.backend.web3_provider_url,
+    #     conf.backend.web3_caller_address,
+    #     conf.backend.web3_private_key,
+    #     log,
+    # )
+    # block_number = web3_client.web3.eth.block_number
+    # log.debug("Config validation block number: {}".format(block_number))
+    # assert block_number is not None, "Failed to get block number from web3 provider"
+
+    """
+    Oxen RPC validations
+    """
+    omq, oxend = omq_connection(conf.backend.rpc_fetcher)
+    rpc = OxenRPC(log, conf.backend.rpc_fetcher, conf.backend.rpc_fetcher_cache)
+    res = rpc.get_info(omq, oxend).get()
+    log.debug("Config validation rpc response status: {}".format(res.get("status")))
+    assert (
+        res is not None and res.get("status") == "OK"
+    ), "Oxen RPC ping to {} failed with response: {}".format(conf.backend.rpc_fetcher, res)
+
+    log.info("Config validation finished")
+    log.perf.end("validate_config")
