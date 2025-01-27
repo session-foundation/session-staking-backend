5ff9b39bedbf2ccd5acad8858e052ad58f58ade3
Ryan Miller <aerilym@pm.me>
2025-01-27 13:44:07 +1100

    fix: performance logger orphan cleanup maths

--- log/perf.py
+++ log/perf.py
@@ -1,5 +1,6 @@
 import logging
 import time
+from copy import copy
 
 
 class PerformanceLogger:
@@ -11,7 +12,7 @@ class PerformanceLogger:
             self.end_timer = self.end_timer_enabled
             self.times = {}
             self.cpu_times = {}
-            self.orphaned_event_age_seconds = 3600  # 1 hour
+            self.orphaned_event_age_ns = 3600 * 10**9  # 1 hour
             self.check_for_orphans_interval = 3600  # 1 hour
             self.last_orphan_prune = 0
         else:
@@ -32,10 +33,12 @@ class PerformanceLogger:
         start_time = self.times.pop(label, None)
         start_time_cpu = self.cpu_times.pop(label, None)
 
-        self._cleanup_orphans()
 
         if start_time is not None and start_time_cpu is not None:
-            elapsed_ms = (time.perf_counter_ns() - start_time) / 1e6
+            process_time_ns = time.perf_counter_ns()
+            self._cleanup_orphans(process_time_ns)
+
+            elapsed_ms = (process_time_ns - start_time) / 1e6
             elapsed_cpu_ms = (time.process_time_ns() - start_time_cpu) / 1e6
             return elapsed_ms, elapsed_cpu_ms
         return None, None
@@ -51,12 +54,13 @@ class PerformanceLogger:
         else:
             self.logger.performance(f"No start time recorded for label '{label}'")
 
-    def _cleanup_orphans(self):
-        now = time.time()
-        if len(self.times) > 0 and now - self.last_orphan_prune > self.check_for_orphans_interval:
-            self.last_orphan_prune = now
-            for label, start in self.times.items():
-                if now - start > self.orphaned_event_age_seconds:
+    def _cleanup_orphans(self, process_time_ns):
+        if len(self.times) > 0 and process_time_ns - self.last_orphan_prune > self.check_for_orphans_interval:
+            self.last_orphan_prune = process_time_ns
+
+            # NOTE: must be a copy as the dictionary is modified during iteration
+            for label, start in copy(self.times).items():
+                if process_time_ns > start + self.orphaned_event_age_ns:
                     self.times.pop(label)
 
     def _noop(self, *args, **kwargs):
