2c739dd46a18b50e9e2f8cd50524c7fceb5c5c71
Ryan Miller <aerilym@pm.me>
2025-01-28 15:30:05 +1100

    chore: move non route methods to app class in price api

--- price/app.py
+++ price/app.py
@@ -25,8 +25,9 @@ class PriceAppConfig(FlaskAppConfig):
 
 
 class App(FlaskApp):
-    def __init__(self, config: PriceAppConfig, name=__name__):
+    def __init__(self, config: PriceAppConfig):
         super().__init__(config)
+        self.app_config = config
 
         if not is_db_initialized(config.sqlite_db):
             self.log.info(
@@ -58,16 +59,62 @@ class App(FlaskApp):
             include_last_updated_at=True,
         )
 
+        self.price_poll_rate_seconds = config.coingecko_api_rate_poll_rate_seconds if config.coingecko_api_rate_poll_rate_seconds is not None else 0
 
-def create_app(config: PriceAppConfig):
+    @staticmethod
+    def get_token_price_cache_key(token: str):
+        return f"price-{token}-all"
+
+    def get_token_info_cached(self, token: str):
+        key = App.get_token_price_cache_key(token)
+
+        data: dict[str, PriceDB] | None = self.cache.get_cached_only(key)
+
+        if data:
+            return data
+
+        data = self.db_reader_prices.get_latest_prices(token)
+
+        updated_at = max(price.updated_at for price in data.values())
+
+        stale_time = updated_at + self.price_poll_rate_seconds
+        self.cache.set_cache_value(key, data, invalidate_timestamp=stale_time)
+        return data
+
+    def get_price_for_token_uncached(self, params: [str, str]):
+        return self.get_token_info_cached(params[0]).get(params[1])
+
+    def get_price_for_token_cached(self, token: str, currency: str) -> PriceDB | None:
+        return self.cache.get(f"price-{token}-{currency}", getter=self.get_price_for_token_uncached,
+                              getter_args=[token, currency], ttl=1)
+
+    def get_token_price_info(self, token: str = None):
+        if token is None:
+            token = self.app_config.default_token
+
+        data = self.get_price_for_token_cached(token, self.app_config.default_currency)
+
+        if data is None:
+            return json_response({"error": "Failed to fetch price"})
+
+        key = App.get_token_price_cache_key(token)
+        stale_time = self.cache.get_stale_timestamp(key)
+
+        return {
+            "usd": data.price,
+            "usd_market_cap": data.market_cap,
+            "t_price": data.updated_at,
+            "t_stale": stale_time,
+        }
+
+
+def create_app(config: PriceAppConfig) -> App:
     app = App(config)
 
     if config.api_rate_limit is not None and config.api_rate_limit_period is not None:
         @app.before_request
         def rate_limit():
             return app.req_limiter.rate_limit()
-
-    price_poll_rate_seconds = config.coingecko_api_rate_poll_rate_seconds if config.coingecko_api_rate_poll_rate_seconds is not None else 0
 
     """
     //////////////////////////////////////////////////////////////
@@ -77,64 +124,22 @@ def create_app(config: PriceAppConfig):
     //////////////////////////////////////////////////////////////
     """
 
-    def get_token_price_cache_key(token: str):
-        return f"price-{token}-all"
-
-    def get_token_info_cached(token: str):
-        key = get_token_price_cache_key(token)
-
-        data: dict[str, PriceDB] | None = app.cache.get_cached_only(key)
-
-        if data:
-            return data
-
-        data = app.db_reader_prices.get_latest_prices(token)
-
-        updated_at = max(price.updated_at for price in data.values())
-
-        stale_time = updated_at + price_poll_rate_seconds
-        app.cache.set_cache_value(key, data, invalidate_timestamp=stale_time)
-        return data
-
-    def get_price_for_token_uncached(params: [str, str]):
-        return get_token_info_cached(params[0]).get(params[1])
-
-    def get_price_for_token_cached(token: str, currency: str) -> PriceDB | None:
-        return app.cache.get(f"price-{token}-{currency}", getter=get_price_for_token_uncached,
-                             getter_args=[token, currency], ttl=1)
-
-    def get_token_price_info(token: str = config.default_token):
-        data = get_price_for_token_cached(token, config.default_currency)
-
-        if data is None:
-            return json_response({"error": "Failed to fetch price"})
-
-        key = get_token_price_cache_key(token)
-        stale_time = app.cache.get_stale_timestamp(key)
-
-        return {
-            "usd": data.price,
-            "usd_market_cap": data.market_cap,
-            "t_price": data.updated_at,
-            "t_stale": stale_time,
-        }
-
     @app.route("/price")
     def route_get_token_price():
         return json_response({
-            "price": get_token_price_info()
+            "price": app.get_token_price_info()
         })
 
     @app.route("/price/<token>")
     def route_get_token_price_for_token(token: str):
         return json_response({
-            "price": get_token_price_info(token)
+            "price": app.get_token_price_info(token)
         })
 
-    if price_poll_rate_seconds > 0:
-        app.log.info("Polling for price info every {} seconds".format(price_poll_rate_seconds))
+    if app.price_poll_rate_seconds > 0:
+        app.log.info("Polling for price info every {} seconds".format(app.price_poll_rate_seconds))
 
-        @timer(price_poll_rate_seconds)
+        @timer(app.price_poll_rate_seconds)
         def fetch_token_price_info(signum):
             app.logger.info("Fetch token price info start")
             data = app.token_price_request.get()
