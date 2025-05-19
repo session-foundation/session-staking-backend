195ff2453e5b1df0b54dcdef77b09ba3b36fcfe2
Ryan Miller <aerilym@pm.me>
2025-05-19 15:58:12 +1000

    feat: add price range endpoint

--- src/price/read.py
+++ src/price/read.py
@@ -15,3 +15,15 @@ def get_latest_price(db_path: str, token: str):
             )
             price = PriceDB(*cursor.fetchone())
             return price
+
+def get_prices_since(db_path: str, token: str, timestamp: int):
+    with closing(sql_connect_in_read_mode(db_path)) as connection:
+        with closing(connection.cursor()) as cursor:
+            cursor.execute(
+                """
+                SELECT * FROM prices WHERE token = ? AND updated_at >= ? ORDER BY updated_at DESC
+                """,
+                (token, timestamp),
+            )
+            prices = [PriceDB(*price) for price in cursor.fetchall()]
+            return prices
